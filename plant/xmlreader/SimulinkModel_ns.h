1b05f3341bbd455999fe64a680f35cef4b998e6b
yoshihiro-ikeda <yoshi12@ertl.jp>
2015-10-30 17:21:10 +0900

    first commit

---
+++ plant/xmlreader/SimulinkModel_ns.h
@@ -0,0 +1,7 @@
+#ifndef SIMULINKMODEL_NS_H
+
+#ifndef SIMULINKMODEL_NS
+#define SIMULINKMODEL_NS "http://example.com/SimulinkModel"
+#endif /* IMULINKMODEL_NS */
+
+#endif /* SIMULINKMODEL_NS_H */
---
+++ plant/xmlreader/xmlreader.hxx
@@ -0,0 +1,6 @@
+#include "SimulinkModel.hxx"
+#include "SimulinkModel_ns.h"
+#include "SimulinkXML.h"
+using namespace std;
+void print_usage(char*);
+void print_blocks(SimulinkModel::XSD::blocks_T &blks);
